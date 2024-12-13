268e64448034aa2c2e216f0a6330e2b11897b2bf
M.Jahanzaib Abid <150152743+Fahad16232@users.noreply.github.com>
2024-12-13 06:05:00 +0500

    Initial Commit

---
+++ Trimmer Track/Trimmer_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trimmer_TrackApp.swift
+//  Trimmer Track
+//
+//  Created by Fahad on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Trimmer_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
